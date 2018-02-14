c9b2c051c0ca2b015715b72cdb2c642aeb54dbde
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 16:25:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,8 @@ def take_a_number(katz_deli, customer)
   puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli, customer)
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty"
+  end
 end
