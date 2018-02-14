8fd16000e58a72c77136dd087bab9a167a808659
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 16:23:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(katz_deli, customer)
   katz_deli << customer
-  puts "Welecome, #{customer}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
 end
